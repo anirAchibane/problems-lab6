8f57b5559a579b673ab8ff8745851cec26ec6bdc
Anir <anirf42@gmail.com>
2025-11-02 16:40:07 +0100

    Solved exercises

--- src/problem1/Shop.java
+++ src/problem1/Shop.java
@@ -21,9 +21,19 @@ public class Shop
             itemPrice = scan.nextDouble();
             System.out.print ("Enter the quantity: ");
             quantity = scan.nextInt();
-// *** create a new item and add it to the cart
+            // *** create a new item and add it to the cart
+            item = new Item(itemName,itemPrice,quantity);
+            cart.add(item);
             // *** print the contents of the cart object using println
+            double totalPrice = 0.0;
+            System.out.println("Cart items: {");
+            for (Item it : cart){
+                System.out.println(it + ",");
+                totalPrice += it.getPrice() * it.getQuantity();
+            }
+            System.out.println("}\nTotal price = $" + totalPrice);
             System.out.print ("Continue shopping (y/n)? ");
+            scan.nextLine();
             keepShopping = scan.nextLine();
         }
         while (keepShopping.equals("y"));
